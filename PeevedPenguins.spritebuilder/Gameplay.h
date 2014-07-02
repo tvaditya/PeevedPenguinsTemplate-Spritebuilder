ba9da9e0c0aa2574a57f9cc281ed9becd3481ec8
tvaditya <chikki@adityas-MacBook-Pro.local>
2014-07-02 18:22:16 -0300

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Gameplay.h
@@ -0,0 +1,13 @@
+//
+//  Gameplay.h
+//  PeevedPenguins
+//
+//  Created by aditya on 7/2/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface Gameplay : CCSprite
+
+@end
