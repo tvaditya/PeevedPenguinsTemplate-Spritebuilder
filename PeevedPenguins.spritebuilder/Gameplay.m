0bbcd15f8f33228b450a6d8fcadbfd8e8daefddb
tvaditya <chikki@adityas-MacBook-Pro.local>
2014-07-04 02:20:54 -0300

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Gameplay.m
+++ PeevedPenguins.spritebuilder/Gameplay.m
@@ -13,6 +13,7 @@
     CCNode *_catapultArm;
     CCNode *_levelNode;
     CCNode *_contentNode;
+    CCNode *_pullbackNode;
 }
 // is called when CCB file has completed loading
 - (void)didLoadFromCCB {
@@ -25,6 +26,9 @@
     //Loding Level1
     CCScene *level = [CCBReader loadAsScene:@"Levels/Level1"];
     [_levelNode addChild:level];
+    
+    // nothing shall collide with our invisible nodes
+    _pullbackNode.physicsBody.collisionMask = @[];
     
 }
 
